9b842017e1297dca8fa7b6efacafca86da59f077
Kamilla Costa Souza <kamillacostasouza@hotmail.com>
2020-05-26 12:03:35 -0300

    Utilizando Body como parametro

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const express = require('express');
 // criando variável que vai armazenar toda a aplicação
 const app = express();
 
+//Especifica que o corpo da requisição esta no formato json
+app.use(express.json());
+
 // Tipos de Parametros
 
 // Query: Parametros nomeados na rota apos '?', serve para filtros e paginação (para usar mais de um filtro usa o &)
@@ -12,18 +15,20 @@ const app = express();
 
 // Query: const params = request.Query
 // Route: const params = request.params
-// Body: const body = request.body
+// Body: const body = request.body (Usa Post no formato json)
 
 // Especificando rotas
 //Request: Guarda todos os dados que vem por meio da requisição
 //Response: Responsável por retornar uma resposta para o usuário
 
-app.get('/users/:id', (request, response) => {
-    const params = request.params;
+app.post('/users', (request, response) => {
+    //especificar no inicio do arq que esta utilizando json no corpo das requisições
+    const body = request.body;
 
-//rota no insominia: http://localhost:3333/users/1
+//rota no insominia: http://localhost:3333/users (Lembre-se que o método é o post e deve se adc o corpo)
 
-    console.log(params);
+    //Para printar no terminal o que foi criado
+    console.log(body);
 
     //Retornando resposta em formato json
     return response.json({
