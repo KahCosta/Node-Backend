30b99380fa966d8ad6863748679d8c2da02745df
Kamilla Costa Souza <kamillacostasouza@hotmail.com>
2020-05-26 11:32:47 -0300

    Explicando sobre Rotas e Parametros

--- index.js
+++ index.js
@@ -4,8 +4,22 @@ const express = require('express');
 // criando variável que vai armazenar toda a aplicação
 const app = express();
 
+// Tipos de Parametros
+
+// Query: Parametros nomeados na rota apos '?', serve para filtros e paginação
+// Route: Parametros utilizados para identificar recursos , :ID(quando quero buscar um usuário específico)
+// Request Body: Corpo da requisição utilizado para criar ou alterar recursos
+
+// Query: const params = request.Query
+// Route: const params = request.params
+// Body: const body = request.body
+
 // Especificando rotas
+//Request: Guarda todos os dados que vem por meio da requisição
+//Response: Responsável por retornar uma resposta para o usuário
+
 app.get('/', (request, response) => {
+    //Retornando resposta em formato json
     return response.json({
         Aluna: 'Kamilla Costa',
         Idade: '22 anos',
